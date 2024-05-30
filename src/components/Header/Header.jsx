8c83ba4b4f3f21ce35735111880f69bfae489273
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-05-30 23:49:55 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,41 @@
+import { NavLink } from 'react-router-dom';
+import css from './Header.module.css';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={css.header}>
+      <nav className={css.navbar}>
+        <NavLink className={css.nanny_link} to="/">
+          Nanny.Services
+        </NavLink>
+        <ul className={css.nav_menu}>
+          <li className={css.li_home}>
+            <NavLink className={css.link_item} to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={css.link_item} to="/nannies">
+              Nannies
+            </NavLink>
+          </li>
+          <li className={css.login_li}>
+            <button className={css.login_btn} aria-controls="loginModal">
+              Login
+            </button>
+          </li>
+          <li>
+            <button
+              className={css.register_btn}
+              aria-controls="registrationModal"
+            >
+              Registration
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Container from '../../components/Container/Container';
+import Header from '../../components/Header/Header';
 import css from './Home.module.css';
 
 const Home = () => {
   return (
     <Container>
+      <Header />
       <div className={css.wrapper_bg}>
         <div className={css.bg_red}></div>
         <div className={css.bg_img}></div>
