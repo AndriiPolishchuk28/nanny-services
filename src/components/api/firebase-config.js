d2fedee95a3e2bffd73762721d0c3b63f40d9c6a
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-06-05 15:50:48 +0200

    firebase config

---
+++ src/components/api/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCAAfmD3bQJTcgQpHkW3uR1cgQwzcq8Y7c',
+  authDomain: 'nannies-a4e76.firebaseapp.com',
+  projectId: 'nannies-a4e76',
+  storageBucket: 'nannies-a4e76.appspot.com',
+  messagingSenderId: '162107724345',
+  appId: '1:162107724345:web:46a5ad5ffee135a496f3e8',
+  measurementId: 'G-PYKGD86H74',
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
