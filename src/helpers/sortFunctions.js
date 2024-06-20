3f80d4360eab034ba4145be779328fe712a114fe
AndriiPolishchuk28 <andrey.work28@gmail.com>
2024-06-21 00:12:45 +0200

    select

--- src/helpers/sortFunctions.js
+++ src/helpers/sortFunctions.js
@@ -1,15 +1,26 @@
 const sort = (array, filter) => {
   switch (filter) {
     case 'ascName':
-      array.sort((a, b) => a.name.localeCompare(b.name));
-      break;
+      return array.sort((a, b) => a.name.localeCompare(b.name));
     case 'descName':
-      array.sort((a, b) => b.name.localeCompare(a.name));
-      break;
+      return array.sort((a, b) => b.name.localeCompare(a.name));
+    case 'cheaper10':
+      return array
+        .filter((elem) => elem.price_per_hour < 10)
+        .sort((a, b) => b.price_per_hour - a.price_per_hour);
+    case 'greater10':
+      return array
+        .filter((elem) => elem.price_per_hour > 10)
+        .sort((a, b) => a.price_per_hour - b.price_per_hour);
+    case 'popular':
+      return array.sort((a, b) => b.rating - a.rating);
+    case 'notpopular':
+      return array.sort((a, b) => a.rating - b.rating);
+    case 'all':
+      return array;
     default:
-      break;
+      return array;
   }
-  return array;
 };
 
 export default sort;
